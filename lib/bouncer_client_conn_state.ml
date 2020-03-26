51260b0cb7b6b04502bc91c86569722c28329aec
hark <hark@puscii.nl>
2020-03-26 12:29:01 +0000

    stuff

--- lib/bouncer_client_conn_state.ml
+++ lib/bouncer_client_conn_state.ml
@@ -15,30 +15,146 @@ type encrypt_msg_error =
 
 type serialized_state = string
 
-type state_get = unit -> (serialized_state, Mirage_kv.error) Stdlib.result Lwt.t
-type state_put =  serialized_state -> (unit, Mirage_kv.write_error) Stdlib.result Lwt.t
-
+let proxy_out = stdout
 (*
 type state_get = unit -> unit Lwt.t
 
 type state_put =  serialized_state -> (string, error) result Lwt.t
+*
 *)
-(* the state of 1 tls session via a bouncer *)
-type t =
-  { mutable tls_state : Tls.Engine.state
-  ; mutable outgoing  : (int64 * string) list
-  ; mutable unencrypted_outgoing : string list
+module Remote = struct
+type t = {
   ; address           : string
   ; port              : int
   ; hostname          : string
-  ; mutable max_covered_sequence : int64
-  ; queue_to_bouncer : string Ke.Fke.t
-  ; queue_from_bouncer : string  Ke.Fke.t
-  ; channel_to_client : string Stream.t
-  ; channel_from_client : string Stream.t
-  ; state_store_get : state_get
-  ; state_store_put : state_put
+}
+end
+
+used_multiplex_ids = Set.
+
+module BouncerIO = struct
+  (* queue for bouncerio of a single connection, the data channel *) 
+
+  type t = {
+    ; queue_to_bouncer : string Ke.Fke.t
+    ; queue_from_bouncer : string  Ke.Fke.t
   }
+
+  let send_bouncer_data m = Ke.Fke.push t.queue_to_bouncer m 
+  let read_bouncer_data = Ke.Fke.pop t.queue_to_bouncer
+
+end
+
+
+module ClientIO struct
+
+   type state_get = unit -> (serialized_state, Mirage_kv.error) Stdlib.result Lwt.t
+   type state_put =  serialized_state -> (unit, Mirage_kv.write_error) Stdlib.result Lwt.t
+
+  type t = {
+    ; channel_to_client : string Stream.t
+    ; channel_from_client : string Stream.t
+    ; state_store_get : state_get
+    ; state_store_put : state_put
+  }
+
+  let send_bouncer_control = Ke.Fke.push t.queue_to_bouncer m 
+  let read_bouncer_control m = Ke.Fke.pop t.queue_to_bouncer
+  let write_client m =
+  let read_client m =  
+
+end
+
+(* the state of 1 tls session via a bouncer *)
+type t =
+  { conn_id : Int32.t
+  ; target : Remote.t
+  ; mutable tls_state : Tls.Engine.state
+  ; mutable outgoing  : (int64 * string) list
+  ; mutable unencrypted_outgoing : string list
+  ; mutable max_covered_sequence : int64
+  ; client_in
+  ; proxy_out 
+  ; outgoing
+  ; io : IO.t
+  }
+
+(* ask the proxy to setup a connection to remote, takes a Target.t returns a IO.t*)
+let setup_bouncer_data_channel remote io = 
+    (* Ask the proxy to connect to target.address: *)
+    (* give it a multiplex id  TODO: extend the connect message to include the multiplex id*)
+    let id = 1234 in
+
+    
+
+    begin match Shared.serialize_connect 20 (remote.address, remote.port) with
+      | None ->
+        Logs.err (fun m ->
+            m "error: unable to serialize connect to \
+               '%s':%d" remote.address remote.port) ;
+        Lwt.return_unit
+      | Some connect_msg ->
+        (* send the connect message on bouncer control channel *)
+        send_bouncer_control connect_msg
+    end
+
+(* setup the tls session via proxy to remote *) 
+let setup_tls_to_remote io remote =
+  let authenticator =
+              x509_fingerprint_authenticator_ignoring_time target.address
+               target.username in
+    Lwt.return @@ Tls.Config.client
+      ~peer_name:target.address (* TODO not ideal for e.g. onions?
+                                   this is currently here so that we can
+                                   reconstruct the Authenticator in
+                                   Tlsping.deserialize_tls_state
+                                *)
+      ~authenticator
+      ~ciphers:[ `TLS_DHE_RSA_WITH_AES_256_CBC_SHA256
+               ; `TLS_DHE_RSA_WITH_AES_256_CBC_SHA ]
+      ~version:Tls.Core.(TLS_1_2 , TLS_1_2) (* g *)
+      ~hashes:[`SHA256 ; `SHA1] ()
+    >>= fun tls_config ->
+       (* [client client] is [tls * out] where [tls] is the initial state,
+          and [out] the initial client hello  *)
+    Lwt return @@ Tls.Engine.client tls_config
+        >>= fun (tls_state, client_hello) ->
+          (* Initiate a TLS connection: *)
+
+
+    (* save the stuff in type t and return it *)
+    let newt {  tls_state
+              ; outgoing = [] ;
+              ; target = target;
+              ; io = io
+              ; unencrypted_outgoing = [] 
+              ;  max_covered_sequence = 0L } in
+(* send the client_hello to proxy *)
+  Lwt_io.write proxy_out (serialize_outgoing t.conn_id 0L
+                            Cstruct.(to_string client_hello)
+                         ) >>= fun () ->
+  Lwt_io.flush proxy_out >>= fun () ->
+  (* setup thread that encrypts outgoing messages by proxy *)
+  Lwt.async (handle_outgoing t.conn_id t.client_in t.proxy_out) ;
+  return @@ `Established
+
+let remove_acked_message ~t =
+    (* message was sent; remove it from local buffer *)
+    let outgoing =
+      let rec f acc = function
+        | (s , _ ) :: tl when -1 = Int64.compare s acked_seq ->
+          f acc tl
+        | hd :: tl -> f (hd::acc) tl
+        | [] -> List.rev acc
+      in f [] t.outgoing
+    in
+    t.outgoing <- outgoing ;
+    Logs.warn (fun m -> m "outgoing ack: TODO cleanup buffers") ;
+    return `Established
+
+
+(*   --  *)
+
 
 let to_string t =
   "bouncer_client_conn_state with: " ^ t.address 
@@ -52,6 +168,122 @@ let checkpoint_state ~t =
    t.state_store_get () >|= function
    | Ok s -> Shared.deserialize_tls_state s
    |  _ -> print_string "state_get failed!!!"; t.tls_state
+
+let handle_outgoing conn_id client_in proxy_out () ~t =
+  (* Read from client, send to bouncer *)
+  let rec loop () =
+    (* TODO handle disconnect / broken line: *)
+    (Lwt_io.read_line client_in >|= fun line ->
+    (* when using Lwt_io.read instead:
+       if "" = line then raise End_of_file ; *)
+    let line = line ^ "\r\n" in
+    t.unencrypted_outgoing <-
+      t.unencrypted_outgoing @ [line] ) >>= fun () ->
+    let rec wait_for_encryption first =
+      match t.tls_state.encryptor with
+      | None ->
+        if first then
+          Logs.warn (fun m -> m "outgoing: queuing since no encryptor state") ;
+        Lwt_unix.sleep 0.5 >>= fun () -> wait_for_encryption false
+      | Some _ -> Lwt.return ()
+    in wait_for_encryption true >>= fun () ->
+    let target_sequence = match t.tls_state.encryptor with
+      | None -> failwith "cannot happen?" | Some {sequence; _} -> sequence in
+    begin match encrypt_queue t.tls_state
+                  (t.unencrypted_outgoing)
+                  target_sequence with
+    | Ok ( tls_state , msg_list ) ->
+      t.unencrypted_outgoing <- [] ;
+      t.tls_state <- tls_state ;
+      let _ = checkpoint_states () in
+      let serialized_msgs =
+        List.map (fun (msg_sequence, cout, `Plaintext plaintext) ->
+            t.outgoing <-
+              (msg_sequence , plaintext) :: conn_state.outgoing ;
+            serialize_outgoing t.conn_id msg_sequence Cstruct.(to_string cout)
+          ) msg_list |> String.concat ""
+      in
+      (*TODO: if this fails, wait for reconnect: *)
+      Lwt_io.write t.proxy_out serialized_msgs >>= fun() ->
+      send_pings_if_needed ~t
+    | Error _ ->
+      Logs.err (fun m -> m "Unable to encrypt and send outgoing message");
+      Lwt.return_unit
+    end
+    >>= fun () -> loop ()
+  in
+  Lwt.catch loop
+    (function End_of_file -> Logs.err (fun m -> m "%s: EOF" __LOC__); return ()
+            | e -> raise e)
+
+let handle_incoming ~next_seq ~queued_seq ~msg ~t =
+  (* proxy_out is the connection tls_ping_server;
+     client_out is the connection to the IRC client *)
+  (* TODO decrypt ; handle write errors; buffer if client disconnected? *)
+  begin match Tls.Engine.handle_tls t.tls_state
+                Cstruct.(of_string msg) with
+  | `Ok (`Ok tls_state , `Response resp, `Data msg) ->
+    begin match t.tls_state.encryptor with
+      | Some {cipher_st ; sequence } ->
+        t.tls_state
+        <- {tls_state with
+            encryptor = Some
+                ({cipher_st ;
+                  sequence = Tlsping.int64_max sequence
+                      (if next_seq <> Int64.max_int
+                       then next_seq else 0L)
+                 }:Tls.State.crypto_context)}
+      | None ->
+        t.tls_state <- tls_state
+    end ;
+    t.max_covered_sequence <- int64_max queued_seq
+        t.max_covered_sequence ;
+    let _ = checkpoint_states () in
+    begin match resp with
+      | Some resp_data ->
+        let sequence = begin match t.encryptor with
+          | Some crypto_context -> crypto_context.sequence
+          | None-> failwith "TODO no encryption context in tls_state" end in
+        Logs.debug (fun m -> m "Upstream: need to transmit") ;
+        Lwt_io.write t.proxy_out (serialize_outgoing t.conn_id sequence
+                                  Cstruct.(to_string resp_data))
+        >>= fun () -> Lwt_io.flush proxy_out
+      | None -> return ()
+    end
+    >>= fun() ->
+    begin match msg with
+      | Some msg_data ->
+        (* TODO
+           String.index_from ":server.example.org PONG server.example.org :TLSPiNG:10" 0 ' '
+           let first_space = 1 + String.index_from msg_data 0 ' ' in
+           let second_space = String.index_from msg_data first_space ' ' in
+           let server = String.sub msg_data 1 (first_space -2) in
+           match String.sub msg_data first_space (second_space - first_space) with
+           | "PONG :TLSPiNG:"
+        *)
+        Logs.debug (fun m ->
+            m "Incoming: remote next_seq: %Ld queued_seq: %Ld"
+              next_seq queued_seq) ;
+        Lwt_io.write client_out Cstruct.(to_string msg_data)
+        >>= fun () -> send_pings_if_needed ~t
+      | None ->
+        Logs.debug (fun m -> m "Upstream: INCOMING: NO MSGDATA");
+        Lwt.return_unit
+    end
+    >>= fun () -> return @@ `Established
+  | `Ok (`Alert typ, `Response resp , `Data msg) ->
+    let _ = resp , msg in (*TODO*)
+    Lwt.return (`Fatal (Fmt.strf "Upstream TLS ALERT %s"
+                        @@ Tls.Packet.alert_type_to_string typ))
+  | `Ok (`Eof , `Response resp , `Data msg) ->
+    let _ = resp , msg in (*TODO*)
+    Lwt.return (`Fatal "Upstream TLS EOF")
+  | `Fail (failure , `Response resp) ->
+    let _ = resp in (*TODO*)
+    Lwt.return @@ `Fatal (Printf.sprintf "Upstream TLS FAIL: %s"
+                          @@ Tls.Engine.string_of_failure failure)
+  end
+
 
 
 let encrypt_queue tls_state payloads seq_num_offset =
--- lib/test_bouncer_client_conn_state.ml
+++ lib/test_bouncer_client_conn_state.ml
@@ -1,16 +1,74 @@
 open Alcotest
+(* let cs = Bouncer_client_conn_state.t  in *)
 
-let cs = Bouncer_client_conn_state 
+open Lwt.Infix
+module KV = Mirage_kv_unix
+let lwt_run f () = Lwt_main.run (f ())
+let do_or_fail = Rresult.R.get_ok
 
-let test_serialization () = 
-  (check string) "serializing 1" "" ""
+let failf fmt = Fmt.kstrf Alcotest.fail fmt
+let assert_fail e = failf "%a" KV.pp_error e
+let assert_write_fail e = failf "%a" KV.pp_write_error e
+(*
+let test_kv () =
+  
+  let%lwt  kv =  Mirage_kv_unix.connect "/tmp/" in
+  let key = Mirage_kv.Key.v "testfile.txt" in
+  let set = (Mirage_kv_unix.set kv key) in
+  let get = (Mirage_kv_unix.get kv key) in
+  let str1 = "Dit is de string" in
+  let str2 = 
+    Lwt.map (function
+      | Ok data -> data
+      | Error e -> Mirage_kv.pp_error e)
+        get
+     in
+     set str1
+     (check string) "serializing 1" str1 (Lwt_main.run str2);
+    Lwt.return_unit
+*)
 
+let get_kv dir file =
+   Mirage_kv_unix.connect "/tmp/" >>= fun kv ->
+  let key = Mirage_kv.Key.v "testfile.txt" in
+  let set = (Mirage_kv_unix.set kv key) in
+  let get = (Mirage_kv_unix.get kv key) in
+  let exists = (Mirage_kv_unix.exists kv key) in
+  Lwt.return (set, get, exists) 
 
-(*  Alcotest.(check string) "items in map" "whtat is dit" serialized_state () *)
+let test_kv () =
+(*
+  Mirage_kv_unix.connect "/tmp/" >>= fun kv ->
+
+  let key = Mirage_kv.Key.v "testfile.txt" in
+  let set = (Mirage_kv_unix.set kv key) in
+  let get = (Mirage_kv_unix.get kv key) in
+   *)
+  let%lwt (set, get, exists) = get_kv "/tmp/" "testfile2.txt" in
+  let str1 = "Dit is de string" in
+  set str1 >|= do_or_fail >>= fun () ->
+  exists >>= function
+  | Error e ->
+    failf "Exists on an existing file failed %a" KV.pp_error e
+  | Ok None ->
+    failf "Exists on an existing file returned None"
+  | Ok (Some `Dictionary) ->
+    failf "Exists on an existing file returned a dictionary"
+  | Ok (Some `Value) ->
+    get >|= do_or_fail >>= fun str2 ->
+    Alcotest.(check string) __LOC__ str1 str2;
+    Lwt.return_unit
+
 
 
 let () =
 
-  run "Utils" [
-      "map", [ test_case "serialization" `Quick test_serialization  ];
+  lwt_run test_kv ();
+  (*
+  run "Connstate" [
+    "kv", [ 
+      "serialization" `Quick lwt_run test_kv;  
+
+          ];
     ]
+     *)
